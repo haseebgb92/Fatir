0c0c223fbedd33350e4020ab64f5e32063095873
Muhammad Haseeb <haseeb.dlp@gmail.com>
2026-09-25 15:09:39 +0500

    chore(companion): bump cloud relay build to 0.2.0

--- companion-android/app/build.gradle.kts
+++ companion-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fatir.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1-local"
+        versionCode = 3
+        versionName = "0.2.0-cloud"
     }
 
     buildFeatures {
