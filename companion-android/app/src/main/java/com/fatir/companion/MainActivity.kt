b442d34c62b785912a2578f6ace451ce4fd9712c
Muhammad Haseeb <haseeb.dlp@gmail.com>
2026-09-25 20:56:57 +0500

    Fix Chat files horizontal scrolling import

--- companion-android/app/src/main/java/com/fatir/companion/MainActivity.kt
+++ companion-android/app/src/main/java/com/fatir/companion/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
