5e7a85427c26ac97882532bba0e762f78f6b0cc1
Muhammad Haseeb <haseeb.dlp@gmail.com>
2026-09-25 21:05:12 +0500

    Fix Chat files scroll state import

--- companion-android/app/src/main/java/com/fatir/companion/MainActivity.kt
+++ companion-android/app/src/main/java/com/fatir/companion/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
