dfe18d21ff72f13aca6cf22d2eefe7b7024f15be
Muhammad Haseeb <haseeb.dlp@gmail.com>
2026-09-26 14:31:37 +0500

    Bump Companion to Remote Desktop Core v0.5

--- companion-android/app/build.gradle.kts
+++ companion-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fatir.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0-chat-media"
+        versionCode = 6
+        versionName = "0.5.0-remote-desktop-core"
     }
 
     buildFeatures {
