f533e937407da0adbcce3346efbfffffeae154e5
Muhammad Haseeb <haseeb.dlp@gmail.com>
2026-09-26 15:33:22 +0500

    Bump Companion to v0.5.1 remote desktop fix

--- companion-android/app/build.gradle.kts
+++ companion-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fatir.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0-remote-desktop-core"
+        versionCode = 7
+        versionName = "0.5.1-remote-desktop-fix"
     }
 
     buildFeatures {
